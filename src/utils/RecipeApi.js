b673b29576a72ca4f55298d4f0fe2903cf4cf268
jakanoh <jakanoh17@gmail.com>
2024-07-14 16:35:25 -0400

    finish api logic

--- src/utils/RecipeApi.js
+++ src/utils/RecipeApi.js
@@ -1,11 +1,19 @@
-class RecipeApi {
-  constructor(baseUrl) {
-    this.baseUrl = baseUrl;
+export default class RecipeApi {
+  constructor(query) {
+    this.params = new URLSearchParams({
+      type: "public",
+      q: query,
+      app_id: "3286f965",
+      app_key: "7a19b6ea3ce9defec65341f59ea7794a",
+    });
   }
 
   getRecipe() {
-    return fetch(this.baseUrl + "/api/recipes/v2", {
-      headers: { "Content-Type": "application/json" },
+    return fetch(`https://api.edamam.com/api/recipes/v2?${this.params}`, {
+      headers: {
+        accept: "application/json",
+        "Accept-Language": "en",
+      },
     }).then((res) => {
       if (res.ok) {
         return res.json();
